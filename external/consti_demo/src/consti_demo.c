5a47369c3f5b13575bad17df6d659b8610eb8e51
consti10 <geierconstantinabc@gmail.com>
2021-12-29 00:09:08 +0100

    use pcap in consti_demo, add wifibroadcast module

--- external/consti_demo/src/consti_demo.c
+++ external/consti_demo/src/consti_demo.c
@@ -3,6 +3,25 @@
 
 #include <sodium.h>
 
+#include <pcap/pcap.h>
+
+static void iteratePcapTimestamps(pcap_t* ppcap){
+        int* availableTimestamps;
+        const int nTypes=pcap_list_tstamp_types(ppcap,&availableTimestamps);
+        //std::cout<<"N available timestamp types "<<nTypes<<"\n";
+        for(int i=0;i<nTypes;i++){
+            const char* name=pcap_tstamp_type_val_to_name(availableTimestamps[i]);
+            const char* description=pcap_tstamp_type_val_to_description(availableTimestamps[i]);
+            //std::cout<<"Name: "<<std::string(name)<<" Description: "<<std::string(description)<<"\n";
+            if(availableTimestamps[i]==PCAP_TSTAMP_HOST){
+                //std::cout<<"Setting timestamp to host\n";
+                pcap_set_tstamp_type(ppcap,PCAP_TSTAMP_HOST);
+            }
+        }
+        pcap_free_tstamp_types(availableTimestamps);
+}
+
+
 int main(int argc, char *argv[])
 {
         printf("Hello World Consti! %d \n",crypto_box_NONCEBYTES);
---
+++ wifibroadcast.mk
@@ -0,0 +1,31 @@
+##################################################
+###########
+#
+### firefly_demo
+#
+##################################################
+###########
+ifeq ($(BR2_PACKAGE_WIFIBROADCAST), y)
+
+        WIFIBROADCAST_VERSION:=1.0.0
+        WIFIBROADCAST_SITE=$(TOPDIR)/../external/wifibroadcast/src
+        WIFIBROADCAST_SITE_METHOD=local
+
+define WIFIBROADCAST_BUILD_CMDS
+        $(TARGET_MAKE_ENV) $(MAKE) CC=$(TARGET_CC) CXX=$(TARGET_CXX) -C $(@D)
+endef
+
+define WIFIBROADCAST_CLEAN_CMDS
+        $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) clean
+endef
+
+define WIFIBROADCAST_INSTALL_TARGET_CMDS
+        $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install
+endef
+
+define WIFIBROADCAST_UNINSTALL_TARGET_CMDS
+        $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) uninstall
+endef
+
+$(eval $(generic-package))
+endif
