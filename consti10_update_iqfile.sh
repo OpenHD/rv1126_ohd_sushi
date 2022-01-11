a1b7b9e9c418735209f25e308431721502644216
consti10 <geierconstantinabc@gmail.com>
2022-01-11 01:16:27 +0100

    gc2053 on CSI1 works

--- consti10_update_iqfile.sh
+++ consti10_update_iqfile.sh
@@ -3,3 +3,4 @@
 #adb push external/camera_engine_rkaiq/iqfiles/lol/imx307_MTV4-IR-E-P_40IRC-4MP-F16.xml /oem/etc/iqfiles/imx415_YT10092_IR0147-28IRC-8M-F20.xml
 adb push external/camera_engine_rkaiq/iqfiles/imx415_YT10092_IR0147-28IRC-8M-F20.xml /oem/etc/iqfiles/imx415_YT10092_IR0147-28IRC-8M-F20.xml
 adb push external/camera_engine_rkaiq/iqfiles/gc2053_YT-RV1109-2-V1_40IR-2MP-F20.xml /oem/etc/iqfiles/gc2053_YT-RV1109-2-V1_40IR-2MP-F20.xml
+adb push external/camera_engine_rkaiq/iqfiles/gc2053_CMK-OT1726-PG1_29IR-2MP-F25.xml /oem/etc/iqfiles/gc2053_CMK-OT1726-PG1_29IR-2MP-F25.xml
