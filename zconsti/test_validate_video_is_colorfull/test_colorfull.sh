867fd189aa786f7856a9df1de9a9149c90c43f26
consti10 <geierconstantinabc@gmail.com>
2022-01-28 20:13:47 +0100

    testing

--- zconsti/test_validate_video_is_colorfull/test_colorfull.sh
+++ zconsti/test_validate_video_is_colorfull/test_colorfull.sh
@@ -6,10 +6,9 @@ rm -rf mamamia.h264
 
 adb shell rm -rf /tmp/mamamia.h264
 
-adb shell ./oem/usr/bin/consti_stream -a -w 1280 -h 720 -d rkispp_scale0 --ip_address 192.168.0.13 -b 5 -o /tmp/mamamia.h264 -t 5
+adb shell /oem/RkLunch-stop.sh
 
-# ^D
-#echo "press ctrl+D to finish\n"
+adb shell ./oem/usr/bin/consti_stream -a -w 1280 -h 720 -d rkispp_scale0 --ip_address 192.168.0.13 -b 5 -o /tmp/mamamia.h264 -t 5
 
 echo "Done recording\n"
 
